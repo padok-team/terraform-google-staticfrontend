2a280e564e6424105d117c6fef58abca33836223
Pierre Averous <piaverous@gmail.com>
2022-01-14 16:28:25 +0100

    refactor(outputs): update output to output whole bucket

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "bucket" {
-  description = "The bucket's name."
-  value       = google_storage_bucket.this.name
+  description = "The bucket."
+  value       = google_storage_bucket.this
 }
